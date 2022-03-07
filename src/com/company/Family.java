7ab0780c266d93d76ae08c70eb5d526f428aa3f8
akanksharustagi <ena.akanksha@gmail.com>
2022-03-07 11:54:31 -0500

    added sout

--- src/com/company/Family.java
+++ src/com/company/Family.java
@@ -1,26 +1,82 @@
 package com.company;
-public class Person {
-    private String firstName;
-    private String lastName;
-    private String address;
-    private int age;
-    public Person(){
-        System.out.println( "non parameterized constructor");
+
+import java.util.Arrays;
+
+public class Family {
+    private String fathername;
+    private String motherName;
+    private String daughterName;
+    private String sonName;
+    private int fatherSalary;
+    private int motherSalary;
+    private int sonFee;
+    private int daugherFee;
+    private int Grocery = 500;
+    public Family() {
+        System.out.println("non parameterized constructor");
     }
-    public Person(String firstName, String lastName, String address , int age){
+    public Family(String fathername, String motherName, String daughterName, String sonName, int fatherSalary, int motherSalary, int sonFee, int daugherFee) {
         System.out.println("parametereized constructor");
-        this.firstName= firstName;
-        this.lastName= lastName;
-        this.address= address;
-        this.age= age;
+        this.fathername = fathername;
+        this.motherName = motherName;
+        this.daughterName = daughterName;
+        this.sonName = sonName;
+        this.fatherSalary = fatherSalary;
+        this.motherSalary = motherSalary;
+        this.daugherFee = daugherFee;
+        this.sonFee = sonFee;
     }
-    public String getfullName() {
-        return firstName + " " + lastName;
+
+
+
+    public void getfamilyNames() {
+
+        System.out.println(" father's name is " + this.fathername);
+        System.out.println(" mother's name is " + this.motherName);
+        System.out.println(" daughter's name is " + this.daughterName);
+        System.out.println("son's name is " + this.sonName);
     }
-    public void changeAddress(String address) {
-        this.address = address;
+    public int gettotalFee() {
+        return daugherFee + sonFee;
     }
-    public String getAddress() {
-        return address;
+
+    public void setFathername(String fathername) {
+        this.fathername = fathername;
+    }
+
+    public void setFatherSalary(int fatherSalary) {
+        this.fatherSalary = fatherSalary;
+    }
+
+    public void setMotherSalary(int motherSalary) {
+        this.motherSalary = motherSalary;
+    }
+
+    public void setSonFee(int sonFee) {
+        this.sonFee = sonFee;
+    }
+
+    public void setDaugherFee(int daugherFee) {
+        this.daugherFee = daugherFee;
+    }
+
+    public void setGrocery(int grocery) {
+        Grocery = grocery;
+    }
+
+    public int gettotalSalary() {
+        return fatherSalary + motherSalary;
+    }
+    public int Savings() {
+        return (gettotalSalary()- gettotalFee() - Grocery);
+    }
+    public void checkSavings() {
+         int netSaving = Savings();
+        if (netSaving > 1000) {
+            System.out.println("Congrats! " + fathername + " you saved this much " + Savings());
+        }
+        else {
+            System.out.println(  " Dear " + fathername + "!!" + " Please save more" );
+        }
     }
 }
