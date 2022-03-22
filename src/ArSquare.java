56304aa856f624e2d965ba2da8eb56b8b0d08fe1
akanksharustagi <ena.akanksha@gmail.com>
2022-03-21 21:59:16 -0400

    added sout

---
+++ src/ArSquare.java
@@ -0,0 +1,9 @@
+public class ArSquare {
+    public static void main(String[] args) {
+        int n = 12;
+        int square = n*n;
+        System.out.println("square of the num="+ square);
+        System.out.println(" square of number");
+    }
+}
+
